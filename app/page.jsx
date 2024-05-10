7d229fcea72d6a4f54e617bf2ec87eb4690840cf
JiriDavid <jiridavidpromise@gmail.com>
2024-05-11 01:34:46 +0530

    imporoved the ui

--- app/page.jsx
+++ app/page.jsx
@@ -13,14 +13,17 @@ export default async function Home() {
         <input name="title" type="text" placeholder="add a new todo" className="border rounded-xl bg-transparent focus:outline-none p-2"/>
         <button type="submit" className="bg-green-700 rounded-full p-2">Add Todo</button>
       </form>
-     <ul>
+     <ul className="flex flex-wrap">
      {
         todos.map((todo)=>(
           <li key={todo.id}>
-            <span>{todo.title}</span>
-            <form action={deleteTodo}>
-              <input type="hidden" name="id" value={todo.id} />
-              <button type="submit">Delete todo</button>
+            <form action={deleteTodo} className="bg-gray-700 p-4 rounded-lg m-4 flex justify-between items-center w-[300px]">
+              <div className="flex flex-col space-y-2">
+                <p className="text-lg ">Task:</p>
+                <span>{todo.title}</span>
+                <input type="hidden" name="id" value={todo.id} />
+              </div>
+              <button type="submit" className="bg-red-700 rounded-full px-2">Delete</button>
             </form>
           </li>
         ))
